80a8ad64bc5d479a89c453728bb927f348752274
rogeriocoutoadv <rogerio@rcouto.adv.br>
2026-03-13 12:38:49 -0300

    Update astro.config.mjs - Rcouto

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 // @ts-check
-
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://www.rcouto.adv.br',
 	integrations: [mdx(), sitemap()],
 });
